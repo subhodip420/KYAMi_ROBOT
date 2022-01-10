ef80212873ece6e9556128006563706bd4b0f9a6
AGASTYA <76867197+subhodip420@users.noreply.github.com>
2022-01-10 16:17:30 +0530

    Update handlers.py

--- KYAMi/modules/helper_funcs/handlers.py
+++ KYAMi/modules/helper_funcs/handlers.py
@@ -8,8 +8,8 @@ from pyrate_limiter import (
 from telegram import Update
 from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler
 
-import KYAMi_ROBOT.modules.sql.blacklistusers_sql as sql
-from KYAMi_ROBOT import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
+import KYAMi.modules.sql.blacklistusers_sql as sql
+from KYAMi import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
 if ALLOW_EXCL:
     CMD_STARTERS = ("/", "!")
